6715b93069fd461a5ab393a6228d4485c587f117
amitkumaryadav <amit9sept@gmail.com>
2023-03-17 15:47:33 +0530

    added_new_file

--- packages.py
+++ packages.py
@@ -1,1 +1,7 @@
-
+import os
+import shutil
+print(os.getcwd())
+total, used, free =shutil.disk_usage("/")
+print(f"Total disk space is: {total // (2**30)} GB")
+print(f"Total disk space is: {used // (2**30)} GB")
+print(f"Total disk space is: {free // (2**30)} GB")
