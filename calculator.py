91df8e3108939be328842d312bdebfc43123386c
amitkumaryadav <amit9sept@gmail.com>
2023-03-17 15:36:56 +0530

    added new_file

--- calculator.py
+++ calculator.py
@@ -1,1 +1,18 @@
-
+#calculate adding, subtraction, multiply, division
+num_1 = int(input("Enter a number:"))
+num_2 = int(input("Enter a another number:"))
+choice = input("Enter the operater + - * /")
+if choice == "+":
+    sum = num_1 + num_2
+    print("The sum of number ",sum)
+elif choice == "-":   
+    diff = num_1 - num_2
+    print("The diff of number ",diff)
+elif choice == "*":
+    Multiple = num_1 * num_2
+    print("The Multiple of number ",Multiple)
+elif choice == "/":
+    division = num_1 / num_2
+    print("The division of number ",division)
+else:
+    print("invalid choice") 
