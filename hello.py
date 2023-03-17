225278b509dfa3f1923ee41565b1007122408d7f
amitkumaryadav <amit9sept@gmail.com>
2023-03-17 15:02:06 +0530

    added new file

--- hello.py
+++ hello.py
@@ -1,1 +1,25 @@
+print("Hello Dosto How are you?")
+print("Wellcome to Goranga Tech")
+print("How can i help you ?")
+print("Goranga tech pvt ltd is basically tour and travel software company")
+print("I want achive my dream")
+print("I am a DevOps Engineer.")
+print("I have around 1 year of work experience in devops")
+print("Currently i'm working in Mohesu Enterprise ltd")
+# variavle kaese bana ha, or unka data type kaese likhna ha.
 
+name = "Amit Kumar"
+age = 27
+price = 0.0
+enjoying = True
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(enjoying))
+
+
+channel = "triponcall.com"
+website = "Goranga.com"
+
+print("my channel name is" ,channel)
+print("My website name is" ,website)
