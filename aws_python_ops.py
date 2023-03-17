324bdcc09a43b52c9d7e6adde5cd828aa2479c2b
amitkumaryadav <amit9sept@gmail.com>
2023-03-17 15:48:54 +0530

    added_newfile

--- aws_python_ops.py
+++ aws_python_ops.py
@@ -1,1 +1,11 @@
+import boto3 
 
+s3 = boto3.resource('s3')
+ec2 = boto3.resource('ec2')
+
+c = 0
+for bucket in s3.buckets.all():
+    print(bucket.name)
+    c = c+1
+    
+s3.Bucket("triponcall.com").download_file("Self introduction.txt","Self introduction.txt")
